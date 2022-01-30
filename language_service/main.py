1c9fe13a1ba371363b171007dc2e8efae97d6a1c
David <dbour.developer@gmail.com>
2022-01-30 13:51:02 -0800

    - feature: Skeleton of the application

---
+++ language_service/main.py
@@ -0,0 +1,11 @@
+import os
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Metadata": {
+        "version": os.environ.get("VERSION", "not specified"),
+    }}

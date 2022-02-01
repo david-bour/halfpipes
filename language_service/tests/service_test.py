90353aa6cc7305e235a5627f5c99c34d2cb9e21a
David <dbour.developer@gmail.com>
2022-01-31 20:06:04 -0800

    fix test

--- language_service/tests/service_test.py
+++ language_service/tests/service_test.py
@@ -2,5 +2,5 @@ import pytest
 from main import metadata_service
 
 def test_can_fetch_metadata():
-    expected = {'Metadata': {'version': '', 'environment': ''}}
+    expected = {'Metadata': {'version': '', 'environment': '', 'region': ''}}
     assert expected == metadata_service()

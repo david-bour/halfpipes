e1a32c7e34fb632c002001bb9b81116a11dcb26a
david bour <dbour.developer@gmail.com>
2022-03-15 22:20:02 -0700

    Adding docker-compose

--- language_service/main.py
+++ language_service/main.py
@@ -1,8 +1,19 @@
 import os
 from typing import Optional
 from fastapi import FastAPI
+from datetime import datetime
+from sqlalchemy.orm import Session
+from . import models
+from .database import SessionLocal, engine
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def metadata_service():
     """ Returns metadata """
@@ -11,6 +22,7 @@ def metadata_service():
             'version': os.environ.get('VERSION', ''),
             'environment': os.environ.get('ENVIRONMENT', ''),
             'region': os.environ.get('REGION', ''),
+            'time': datetime.now()
         }
     }
 
